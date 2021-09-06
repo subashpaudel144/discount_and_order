52faf8a294cc8e05174e55b10608be73a231899e
Subash Paudel <subashpaudel144@gmail.com>
2021-09-06 22:57:16 +0545

    all files

---
+++ src/components/Discount.jsx
@@ -0,0 +1,247 @@
+import React, { Component } from 'react';
+import './Discount.css';
+
+export class Discount extends Component {
+    render() {
+        return (
+            <div className='discount'>
+                <div className='topbar'>
+                    <div className="topleft">
+                        <h3>Discount and Order</h3>
+                    </div>
+                    <div className="topright">
+                        <ul>
+                            <li><input type="text" placeholder='search here' /></li>
+                            <li>home</li>
+                            <li>sort by</li>
+                            <li>sort by</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className= 'secondtop'>
+                    <ul>
+                        <li><a href='#all' eventkey='all'>ALL</a></li>
+                        <li><a href='#active' eventkey='active'>Active</a></li>
+                        <li><a href='scheduled' eventkey='scheduled'>Scheduled</a></li>
+                        <li><a href='#expired' eventkey='expired'>Expired</a></li>
+                    </ul>
+                </div>
+                <div className='cards'>
+                    <div className = 'card'>
+                        <div className = 'cardtop'>
+                            <div className='cardtopleft'>
+                                Discount on items(Normal Discount)<br/><br/>
+                                <a > Active</a>
+                            </div>
+                            <div className = 'cardtopright'>
+                                tripledot
+                            </div>
+                        </div>
+                        <div className = 'cardmiddle'>
+                            <div className='cardmiddleleft'>
+                                Valid Through <br/>
+                                15/07/2021-18/07/2021
+
+                            </div>
+                            <div className='cardmiddleright'>
+                                Valid on <br/>
+                                All Week
+
+                            </div>
+                        </div>
+                        <div className='cardbottom'>
+                            <div className='cardbottomleft'>
+                                Type <br/>
+                                20% on non-veg dishes
+
+                            </div>
+                            <div className='cardbottomright'>
+                                Figure
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = 'card'>
+                        <div className = 'cardtop'>
+                            <div className='cardtopleft'>
+                                Discount on items(Normal Discount)<br/><br/>
+                                <a > Active</a>
+                            </div>
+                            <div className = 'cardtopright'>
+                                tripledot
+                            </div>
+                        </div>
+                        <div className = 'cardmiddle'>
+                            <div className='cardmiddleleft'>
+                                Valid Through <br/>
+                                15/07/2021-18/07/2021
+
+                            </div>
+                            <div className='cardmiddleright'>
+                                Valid on <br/>
+                                All Week
+
+                            </div>
+                        </div>
+                        <div className='cardbottom'>
+                            <div className='cardbottomleft'>
+                                Type <br/>
+                                20% on non-veg dishes
+
+                            </div>
+                            <div className='cardbottomright'>
+                                Figure
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = 'card'>
+                        <div className = 'cardtop'>
+                            <div className='cardtopleft'>
+                                Discount on items(Normal Discount)<br/><br/>
+                                <a > Active</a>
+                            </div>
+                            <div className = 'cardtopright'>
+                                tripledot
+                            </div>
+                        </div>
+                        <div className = 'cardmiddle'>
+                            <div className='cardmiddleleft'>
+                                Valid Through <br/>
+                                15/07/2021-18/07/2021
+
+                            </div>
+                            <div className='cardmiddleright'>
+                                Valid on <br/>
+                                All Week
+
+                            </div>
+                        </div>
+                        <div className='cardbottom'>
+                            <div className='cardbottomleft'>
+                                Type <br/>
+                                20% on non-veg dishes
+
+                            </div>
+                            <div className='cardbottomright'>
+                                Figure
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = 'card'>
+                        <div className = 'cardtop'>
+                            <div className='cardtopleft'>
+                                Discount on items(Normal Discount)<br/><br/>
+                                <a > Active</a>
+                            </div>
+                            <div className = 'cardtopright'>
+                                tripledot
+                            </div>
+                        </div>
+                        <div className = 'cardmiddle'>
+                            <div className='cardmiddleleft'>
+                                Valid Through <br/>
+                                15/07/2021-18/07/2021
+
+                            </div>
+                            <div className='cardmiddleright'>
+                                Valid on <br/>
+                                All Week
+
+                            </div>
+                        </div>
+                        <div className='cardbottom'>
+                            <div className='cardbottomleft'>
+                                Type <br/>
+                                20% on non-veg dishes
+
+                            </div>
+                            <div className='cardbottomright'>
+                                Figure
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = 'card'>
+                        <div className = 'cardtop'>
+                            <div className='cardtopleft'>
+                                Discount on items(Normal Discount)<br/><br/>
+                                <a > Active</a>
+                            </div>
+                            <div className = 'cardtopright'>
+                                tripledot
+                            </div>
+                        </div>
+                        <div className = 'cardmiddle'>
+                            <div className='cardmiddleleft'>
+                                Valid Through <br/>
+                                15/07/2021-18/07/2021
+
+                            </div>
+                            <div className='cardmiddleright'>
+                                Valid on <br/>
+                                All Week
+
+                            </div>
+                        </div>
+                        <div className='cardbottom'>
+                            <div className='cardbottomleft'>
+                                Type <br/>
+                                20% on non-veg dishes
+
+                            </div>
+                            <div className='cardbottomright'>
+                                Figure
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = 'card'>
+                        <div className = 'cardtop'>
+                            <div className='cardtopleft'>
+                                Discount on items(Normal Discount)<br/><br/>
+                                <a > Active</a>
+                            </div>
+                            <div className = 'cardtopright'>
+                                tripledot
+                            </div>
+                        </div>
+                        <div className = 'cardmiddle'>
+                            <div className='cardmiddleleft'>
+                                Valid Through <br/>
+                                15/07/2021-18/07/2021
+
+                            </div>
+                            <div className='cardmiddleright'>
+                                Valid on <br/>
+                                All Week
+
+                            </div>
+                        </div>
+                        <div className='cardbottom'>
+                            <div className='cardbottomleft'>
+                                Type <br/>
+                                20% on non-veg dishes
+
+                            </div>
+                            <div className='cardbottomright'>
+                                Figure
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+        )
+    }
+}
+
+export default Discount
